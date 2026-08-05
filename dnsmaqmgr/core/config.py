5f36343b03dc4bb880fb311f145abef099ea6965
root <root@silo.onthenile.net>
2026-08-05 11:52:31 +0000

    Bump version to 0.2.0 (lookup, query log, blocklists, backup/restore)

--- dnsmaqmgr/core/config.py
+++ dnsmaqmgr/core/config.py
@@ -16,7 +16,7 @@ APP_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file
 STATIC_DIR = os.path.join(APP_DIR, 'static')
 TEMPLATES_DIR = os.path.join(APP_DIR, 'templates')
 
-APP_VERSION = '0.1.0'
+APP_VERSION = '0.2.0'
 
 DATA_DIR = os.environ.get('DNSMAQ_DATA_DIR', APP_DIR)
 STATE_DIR = os.path.join(DATA_DIR, 'state')
